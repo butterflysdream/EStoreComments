fbb995ef9c6d9f343c2e4e646d62c9e904f3a4e1
Jie_Hu <bonehj@163.com>
2017-09-20 18:00:17 +0800

    taobaospider

--- CommentsSys/taobaoSpider.py
+++ CommentsSys/taobaoSpider.py
@@ -1,26 +1,59 @@
+import http
 import json
-from urllib import request
+import jieba.posseg as pseg
+from urllib import request,parse
 
 import simplejson
+import time
 
 
-def spider():
-    url = 'https://rate.tmall.com/list_detail_rate.htm?itemId=545655851754&spuId=723713456&sellerId=2129855716';
-    response = request.urlopen(url)
-    content = response.read()
+def spider(url):
+    header = {
+        "Connection": "keep-alive",
+        "Upgrade-Insecure-Requests": "1",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36",
+        "Accept": " text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+        "Accept-Language": "zh-CN,zh;q=0.8"
+    };
+    req = request.Request(url=url,headers=header)
+    response = request.urlopen(req)
+    content = response.read().decode('gbk')
     print(content)
-    data = str(content.decode('raw_unicode_escape'))
-
-    # data.replace("\\r\\n\\r\\n",'',0)
-    # print(data[10:])
+    data = str(content)
     data = '{'+data+'}'
     print(data)
     datajson  = simplejson.loads(data)
     rateDatail = datajson["rateDetail"]
     paginator = rateDatail["paginator"]
     page = paginator['page']
-    print(page)
+    lastPage = paginator['lastPage']
+    for i in range(0,lastPage):
+        time.sleep(30)
+        parm = {}
+        parm['currentPage'] = i+1
+        url_values = parse.urlencode(parm)
+        full_url = url +"&"+ url_values
+        print(full_url)
+        req = request.Request(url=full_url, headers=header)
+        response = request.urlopen(req)
+        content = response.read().decode('gbk')
+        data = str(content)
+        data = '{' + data + '}'
+        print(data)
+        datajson = simplejson.loads(data)
+        rateDatail = datajson["rateDetail"]
+        rateList = rateDatail["rateList"]
+        wordf = ['d','e','f','g','h','p','r','q']
+        for rate in rateList:
+            comm = rate["rateContent"]
+            seg_list = pseg.cut(comm)
+            with open('comm.txt', 'a') as f:
+                for word, flag in seg_list:
+                    if(flag not in wordf):
+                        f.write(word+" ")
+    # print(page)
 
 if __name__ == "__main__":
-    spider();
-    
+    #itemId=534997940553&spuId=644685204&sellerId=890482188
+    url = 'https://rate.tmall.com/list_detail_rate.htm?itemId=534997940553&spuId=644685204&sellerId=890482188';
+    spider(url);
