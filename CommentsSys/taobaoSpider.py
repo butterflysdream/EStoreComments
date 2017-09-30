8015e7f173fc5dc54c050cbde4c4d2cac82e932b
Jie_Hu <bonehj@163.com>
2017-09-30 11:57:10 +0800

    taobaospider

--- CommentsSys/taobaoSpider.py
+++ CommentsSys/taobaoSpider.py
@@ -3,22 +3,39 @@ import json
 import jieba.posseg as pseg
 from urllib import request,parse
 
+import re
+from bs4 import BeautifulSoup
 import simplejson
 import time
 
 
 def spider(url):
+    result = {}
+    itemid = ""
+    pat = r'https://detail.tmall.com/item.htm[?]id=[0-9]+'
+    reg =  re.compile(pattern=pat)
+    if(re.match(reg,url)):
+        itemid = re.findall(re.compile('[0-9]+'),url)[0]
+        print(itemid)
+    else:
+        result['error'] = 'Please enter the correct URL!!'
+        return result
     header = {
         "Connection": "keep-alive",
         "Upgrade-Insecure-Requests": "1",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36",
+        "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36",
         "Accept": " text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
-        "Accept-Language": "zh-CN,zh;q=0.8"
-    };
+        "Accept-Language": "zh-CN,zh;q=0.8",
+        "Accept - encoding": "gzip, deflate, br",
+        "Cookie":"uss=WvcTf%2Fm0FVNmFsJTmyzfORO5c5j7e%2ByfWEBy4a%2FlVsc9fGhbZBLCzdGTPg%3D%3D; _m_h5_tk=02350c868a5edde2bc970cac800368c6_1505893868202; _m_h5_tk_enc=33952373d6576df9977c52b9e9bbccb1; cna=6ddIElTq+hQCAXFshQLlWDhg; otherx=e%3D1%26p%3D*%26s%3D0%26c%3D0%26f%3D0%26g%3D0%26t%3D0; isg=Avj4F8WQ1ZlDFjl5a5uLEG-HyaZKyVVV7xLuCDJq_zOUTYg32nNce8stc3OG; hng=CN%7Czh-CN%7CCNY%7C156; uc1=cookie14=UoTcCfOicyCJkg%3D%3D; uc3=sg2=AVH%2FkEb8%2Bgdy3jLSerMFMambpOr2BLqt5zmUjIVupTs%3D&nk2=AQuN88GK&id2=VAMVeCmJEWbD&vt3=F8dBzWjJcyKPrrWrO5c%3D&lg2=VT5L2FSpMGV7TQ%3D%3D; t=dcf7cbf1344fe32442371f734409967a; tracknick=bonehj; lgc=bonehj; _tb_token_=563ae5137ef55; cookie2=3f19758d9dc2fae097b909005a7262ea"
+    }
     req = request.Request(url=url,headers=header)
     response = request.urlopen(req)
     content = response.read().decode('gbk')
-    print(content)
+    soup = BeautifulSoup(content,"html.parser")
+    item = soup.find(id='dsr-userid')
+    sellerid = item["value"]
+    print(sellerid)
     data = str(content)
     data = '{'+data+'}'
     print(data)
@@ -51,9 +68,11 @@ def spider(url):
                 for word, flag in seg_list:
                     if(flag not in wordf):
                         f.write(word+" ")
-    # print(page)
+    print(page)
 
 if __name__ == "__main__":
     #itemId=534997940553&spuId=644685204&sellerId=890482188
     url = 'https://rate.tmall.com/list_detail_rate.htm?itemId=534997940553&spuId=644685204&sellerId=890482188';
     spider(url);
+
+def get_product_info(url):
